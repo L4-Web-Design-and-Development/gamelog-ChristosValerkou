1866f573123c99b732dc24bb61533231dd7190ed
ChristosValerkou <1253041@hc.ac.je>
2025-04-24 11:47:56 +0100

    created add game basic page with title and started CSS styling
    Also mapped games through to main page

---
+++ app/routes/add-game.tsx
@@ -0,0 +1,7 @@
+export default function AddGame() {
+  return (
+    <div className="container font-bold mx-auto py-10 text-indigo-100 bg-gradient-to-b from-indigo-900 to-indigo-950 p-20">
+      <h1>Add Game</h1>
+    </div>
+  );
+}
